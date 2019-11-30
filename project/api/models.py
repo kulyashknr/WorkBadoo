d9db73b3553e815f643ac78187954807354411ba
Kulyash Konyrova <konyrova@gmail.com>
2019-12-01 01:12:03 +0600

    Revert "models"
    This reverts commit 267a9e8322c291edaaa246b2d5f862e9c23e0e10.

--- project/api/models.py
+++ project/api/models.py
@@ -1,35 +1,3 @@
 from django.db import models
-from users.models import Company, MainUser, Worker
-from . import constants
 
-
-class Vacancy(models.Model):
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now=True)
-    creator = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, related_name='vacancy_creator')
-    description = models.TextField
-    salary = models.IntegerField
-    is_active = models.BooleanField()
-    industry = models.SmallIntegerField(choices=constants.INDUSTRY_TYPES)
-
-    def __str__(self):
-        return f'{ self.name }: { self.salary }, { self.company }'
-
-
-class MatchingForWorker(models.Model):
-    worker = models.ForeignKey(MainUser, on_delete=models.DO_NOTHING, null=True, related_name='matchforworker_worker')
-    vacancies = models.ManyToManyField(Vacancy)
-
-
-class MatchingForCompany(models.Model):
-    workers = models.ManyToManyField(Worker)
-    vacancy = models.ForeignKey(Vacancy, on_delete=models.DO_NOTHING, null=True, related_name='matchforcompany_vacancy')
-
-
-class Matching(models.Model):
-    vacancy = models.ForeignKey(Vacancy, on_delete=models.DO_NOTHING, null=True, related_name='vacancy')
-    worker = models.ForeignKey(MainUser, on_delete=models.DO_NOTHING, null=True, related_name='worker')
-
-
-
-
+# Create your models here.
--- project/users/models.py
+++ project/users/models.py
@@ -1,34 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from rest_framework.authtoken.models import Token
 
 # Create your models here.
-
-
-class MainUser(AbstractUser):
-    #photo = models.FileField(upload_to=user_photo_path, validators=[validate_size, validate_extension], blank=True, null=True)
-
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-
-    def __str__(self):
-        return f'{self.id}: {self.username}'
-
-
-class Worker(models.Model):
-    user = models.OneToOneField(MainUser, on_delete=models.CASCADE, related_name='worker_user')
-    bio = models.TextField(max_length=500)
-    education = models.CharField(max_length=255)
-    experience = models.CharField(max_length=255)
-
-
-class Company(models.Model):
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(MainUser, on_delete=models.CASCADE, null=True, related_name='company_user')
-    description = models.TextField
-    address = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f'{ self.name }: { self.description }'
-
